d0c66f82f088fca2b926ec69b38d048d4222694d
selim679 <salimfekih35@gmail.com>
2026-04-22 21:13:58 +0100

    creation data transfer ojjects

--- src/main/java/com/example/librarymanagment/entity/Author.java
+++ src/main/java/com/example/librarymanagment/entity/Author.java
@@ -1,27 +1,26 @@
 package com.example.librarymanagment.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
 
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Table(name = "authors")
 public class Author {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String nom;
     private String prenom;
     private LocalDate dateNaissance;
     private String nationalite;
 
     @ManyToMany(mappedBy = "authors")
-    private Set<Book> books = new HashSet<>();
+    private List<Book> books = new ArrayList<>();
 }
--- src/main/java/com/example/librarymanagment/entity/Loan.java
+++ src/main/java/com/example/librarymanagment/entity/Loan.java
@@ -1,2 +1,34 @@
-package com.example.librarymanagment.entity;public class Loan {
+package com.example.librarymanagment.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "loans")
+public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate dateEmprunt;
+
+    @Column(nullable = false)
+    private LocalDate dateRetourPrevue;
+
+    // Null si pas encore rendu
+    private LocalDate dateRetourEffective;
+    // Null si pas de pénalité
+    private Double penalite;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
 }

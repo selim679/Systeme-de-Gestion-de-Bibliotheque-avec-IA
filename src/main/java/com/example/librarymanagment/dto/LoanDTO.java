2011fc72e0cc5f04cf12eba26251efe4af89637c
selim679 <salimfekih35@gmail.com>
2026-04-22 21:21:50 +0100

    fix loandto

--- src/main/java/com/example/librarymanagment/dto/LoanDTO.java
+++ src/main/java/com/example/librarymanagment/dto/LoanDTO.java
@@ -1,4 +1,15 @@
 package com.example.librarymanagment.dto;
 
+
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
 public class LoanDTO {
-}
+    private Long id;
+    private Long bookId;
+    private Long memberId;
+    private LocalDate dateEmprunt;
+    private LocalDate dateRetourPrevue;
+    private LocalDate dateRetourEffective;
+    private Double penalite;

bbb67ff2043ddb1bd65da85cd9b5bec52fee59a9
selim679 <salimfekih35@gmail.com>
2026-04-22 21:19:21 +0100

    creation bookdto

--- src/main/java/com/example/librarymanagment/dto/BookDTO.java
+++ src/main/java/com/example/librarymanagment/dto/BookDTO.java
@@ -1,4 +1,17 @@
 package com.example.librarymanagment.dto;
 
+import lombok.Data;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
 public class BookDTO {
+    private Long id;
+    private String titre;
+    private String isbn;
+    private LocalDate datePublication;
+    private String genre;
+    private Integer nombreExemplaires;
+    private Integer disponibles;
+    private List<Long> authorIds;   // IDs des auteurs pour la création
 }

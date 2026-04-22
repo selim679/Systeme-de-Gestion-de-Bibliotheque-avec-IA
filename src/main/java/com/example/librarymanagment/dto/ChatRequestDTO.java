d08ea7f642e38d715ab55b23a84a0a8373798e6f
selim679 <salimfekih35@gmail.com>
2026-04-22 21:25:21 +0100

    fix chatrequestdto

--- src/main/java/com/example/librarymanagment/dto/ChatRequestDTO.java
+++ src/main/java/com/example/librarymanagment/dto/ChatRequestDTO.java
@@ -1,4 +1,8 @@
 package com.example.librarymanagment.dto;
 
+import lombok.Data;
+
+@Data
 public class ChatRequestDTO {
+    private String message;   // Ex: "Je cherche un roman de science-fiction"
 }

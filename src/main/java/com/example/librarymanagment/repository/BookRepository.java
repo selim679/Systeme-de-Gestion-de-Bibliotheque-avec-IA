fa660cbc2c19951cac8fcc4a7e7c8595aed0be7a
selim679 <salimfekih35@gmail.com>
2026-04-23 10:30:26 +0100

    creation bookrepository

--- src/main/java/com/example/librarymanagment/repository/BookRepository.java
+++ src/main/java/com/example/librarymanagment/repository/BookRepository.java
@@ -1,4 +1,12 @@
 package com.example.librarymanagment.repository;
 
+
+import com.library.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
 public class BookRepository {
 }

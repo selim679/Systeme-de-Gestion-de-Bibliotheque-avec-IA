df4d8ccbea563acfd9b39cac8cbf3edfe987e58e
selim679 <salimfekih35@gmail.com>
2026-04-23 10:38:11 +0100

    fix loanrepository

--- src/main/java/com/example/librarymanagment/repository/LoanRepository.java
+++ src/main/java/com/example/librarymanagment/repository/LoanRepository.java
@@ -5,5 +5,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
-public class LoanRepository {
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+
+    // Emprunts en cours (pas encore rendus)
+    @Query("SELECT l FROM Loan l WHERE l.dateRetourEffective IS NULL")
+    List<Loan> findCurrentLoans();
+
+    // Emprunts en retard
+    @Query("SELECT l FROM Loan l WHERE l.dateRetourEffective IS NULL " +
+            "AND l.dateRetourPrevue < CURRENT_DATE")
+    List<Loan> findOverdueLoans();
 }

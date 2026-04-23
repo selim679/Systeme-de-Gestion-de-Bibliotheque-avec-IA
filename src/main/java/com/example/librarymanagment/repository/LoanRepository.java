483a941943134f6f792b57a767b7c022118cd5e2
selim679 <salimfekih35@gmail.com>
2026-04-23 10:37:29 +0100

    impotation loanrepository

--- src/main/java/com/example/librarymanagment/repository/LoanRepository.java
+++ src/main/java/com/example/librarymanagment/repository/LoanRepository.java
@@ -1,4 +1,9 @@
 package com.example.librarymanagment.repository;
+import com.library.entity.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
 public class LoanRepository {
 }

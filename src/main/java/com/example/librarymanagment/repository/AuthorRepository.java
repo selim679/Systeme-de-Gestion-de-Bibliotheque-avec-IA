11dc97fe9b0792681f30446784aa589588fe127d
selim679 <salimfekih35@gmail.com>
2026-04-24 01:02:45 +0100

    fix authorrepository

--- src/main/java/com/example/librarymanagment/repository/AuthorRepository.java
+++ src/main/java/com/example/librarymanagment/repository/AuthorRepository.java
@@ -1,6 +1,6 @@
 package com.example.librarymanagment.repository;
 
-import com.library.entity.Author;
+import com.example.librarymanagment.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;

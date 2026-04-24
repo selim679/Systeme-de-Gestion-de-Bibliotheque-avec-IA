d699ddd206fbffe0559396604543180e95fc3fed
selim679 <salimfekih35@gmail.com>
2026-04-24 01:01:22 +0100

    importation authorrepository

--- src/main/java/com/example/librarymanagment/repository/AuthorRepository.java
+++ src/main/java/com/example/librarymanagment/repository/AuthorRepository.java
@@ -1,4 +1,10 @@
 package com.example.librarymanagment.repository;
 
+import com.library.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
 public class AuthorRepository {
 }

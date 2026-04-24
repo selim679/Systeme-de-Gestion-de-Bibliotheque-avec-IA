924a1162ddbbd1d93dcc4494e96af2b5e654e5be
selim679 <salimfekih35@gmail.com>
2026-04-24 01:08:21 +0100

    importation loanservice

--- src/main/java/com/example/librarymanagment/service/LoanService.java
+++ src/main/java/com/example/librarymanagment/service/LoanService.java
@@ -1,2 +1,15 @@
 package com.example.librarymanagment.service;
 
+
+import com.library.dto.LoanDTO;
+import com.library.entity.*;
+import com.library.repository.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+public class LoanService {
+}

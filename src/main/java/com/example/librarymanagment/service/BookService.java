a0a2a2c58c32e8c84fe9d5e719208d0908c48cd7
selim679 <salimfekih35@gmail.com>
2026-04-25 13:43:49 +0100

    creation bookservice

--- src/main/java/com/example/librarymanagment/service/BookService.java
+++ src/main/java/com/example/librarymanagment/service/BookService.java
@@ -1,4 +1,14 @@
 package com.example.librarymanagment.service;
 
+
+import com.library.dto.BookDTO;
+import com.library.entity.Author;
+import com.library.entity.Book;
+import com.library.repository.AuthorRepository;
+import com.library.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
 public class BookService {
 }

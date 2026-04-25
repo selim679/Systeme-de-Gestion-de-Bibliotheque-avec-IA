6d60bac6dbd22b4c8bab32af0f8072aae7a6489a
selim679 <salimfekih35@gmail.com>
2026-04-25 13:44:23 +0100

    bookservice

--- src/main/java/com/example/librarymanagment/service/BookService.java
+++ src/main/java/com/example/librarymanagment/service/BookService.java
@@ -10,5 +10,43 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class BookService {
-}
+
+    private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getById(Long id) {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Livre non trouvé : " + id));
+    }
+
+    public Book createBook(BookDTO dto) {
+        Book book = new Book();
+        mapDtoToEntity(dto, book);
+        return bookRepository.save(book);
+    }
+
+    public Book updateBook(Long id, BookDTO dto) {
+        Book book = getById(id);
+        mapDtoToEntity(dto, book);
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    public List<Book> searchBooks(String titre, String genre,
+                                  String isbn, String authorName,
+                                  Boolean disponible) {
+        return bookRepository.searchBooks(titre, genre, isbn, authorName, disponible);
+    }
+
+    public List<Book> getTopBorrowed() {
+        return bookRepository.findTopBorrowed();

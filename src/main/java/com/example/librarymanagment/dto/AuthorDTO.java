5d502e43e1dc21dc8a1379e81853b4d907193d54
selim679 <salimfekih35@gmail.com>
2026-04-25 14:04:35 +0100

    creation authordto

--- src/main/java/com/example/librarymanagment/dto/AuthorDTO.java
+++ src/main/java/com/example/librarymanagment/dto/AuthorDTO.java
@@ -1,4 +1,16 @@
 package com.example.librarymanagment.dto;
 
+
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class AuthorDTO {
+    private Long id;
+    private String nom;
+    private String prenom;
+    private LocalDate dateNaissance;
+    private String nationalite;
 }

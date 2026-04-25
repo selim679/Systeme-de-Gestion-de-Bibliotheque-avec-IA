a4594cb28317b53abc08a2e45c3e0a3ab7c3b496
selim679 <salimfekih35@gmail.com>
2026-04-25 14:06:55 +0100

    creation memberdto

--- src/main/java/com/example/librarymanagment/dto/MemberDTO.java
+++ src/main/java/com/example/librarymanagment/dto/MemberDTO.java
@@ -1,4 +1,17 @@
 package com.example.librarymanagment.dto;
 
+
+
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class MemberDTO {
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String email;
+    private LocalDate dateAdhesion;
 }

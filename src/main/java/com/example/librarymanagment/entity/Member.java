3923fb7ef98e3ed3d8bcb0e2dfce67a43bb94ce3
selim679 <salimfekih35@gmail.com>
2026-04-26 21:26:33 +0100

    update entity member

--- src/main/java/com/example/librarymanagment/entity/Member.java
+++ src/main/java/com/example/librarymanagment/entity/Member.java
@@ -1,30 +1,29 @@
 package com.example.librarymanagment.entity;
 
+
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
-@Table(name = "members")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false)
     private String nom;
-
     private String prenom;
-
-    @Column(unique = true, nullable = false)
+    @Column(unique = true)
     private String email;
-
     private LocalDate dateAdhesion;
 
-    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
-    private List<Loan> loans = new ArrayList<>();
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Loan> loans = new HashSet<>();
 }

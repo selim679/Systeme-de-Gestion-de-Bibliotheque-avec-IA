1b2bfed585125e8fd5449a4531e328b25a537eb7
selim679 <salimfekih35@gmail.com>
2026-04-26 21:45:29 +0100

    update LoanRepository

--- src/main/java/com/example/librarymanagment/repository/LoanRepository.java
+++ src/main/java/com/example/librarymanagment/repository/LoanRepository.java
@@ -1,19 +1,20 @@
 package com.example.librarymanagment.repository;
+
+
+import com.example.librarymanagment.entity.Author;
 import com.example.librarymanagment.entity.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
+    List<Loan> findByDateRetourEffectiveIsNull(); // Emprunts en cours
+    List<Loan> findByDateRetourEffectiveIsNullAndDateRetourPrevueBefore(LocalDate currentDate); // Emprunts en retard
 
-    // Emprunts en cours (pas encore rendus)
-    @Query("SELECT l FROM Loan l WHERE l.dateRetourEffective IS NULL")
-    List<Loan> findCurrentLoans();
-
-    // Emprunts en retard
-    @Query("SELECT l FROM Loan l WHERE l.dateRetourEffective IS NULL " +
-            "AND l.dateRetourPrevue < CURRENT_DATE")
-    List<Loan> findOverdueLoans();
+    @Query("SELECT a FROM Author a JOIN a.books b JOIN b.loans l GROUP BY a ORDER BY COUNT(l) DESC")
+    List<Author> findTopPopularAuthors();
 }

877c4537d3f583767da075cf998d531871186737
selim679 <salimfekih35@gmail.com>
2026-04-26 21:43:31 +0100

    update AuthorRepository

--- src/main/java/com/example/librarymanagment/repository/AuthorRepository.java
+++ src/main/java/com/example/librarymanagment/repository/AuthorRepository.java
@@ -1,15 +1,10 @@
 package com.example.librarymanagment.repository;
 
+
 import com.example.librarymanagment.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-
-    @Query("SELECT a FROM Author a LEFT JOIN a.books b LEFT JOIN b.loans l " +
-            "GROUP BY a ORDER BY COUNT(l) DESC")
-    List<Author> findMostPopular();
 }

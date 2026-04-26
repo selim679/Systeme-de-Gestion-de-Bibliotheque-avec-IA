f342ddea0bfc9e30d7c958c0e540783def5b5b45
selim679 <salimfekih35@gmail.com>
2026-04-26 21:35:53 +0100

    creation LoanReturnDTO

--- src/main/java/com/example/librarymanagment/dto/LoanReturnDTO.java
+++ src/main/java/com/example/librarymanagment/dto/LoanReturnDTO.java
@@ -1,4 +1,10 @@
-package com.example.librarymanagment.dto;
+package com.example.bibliotheque.dto;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class LoanReturnDTO {
+    private LocalDate dateRetourEffective;
 }

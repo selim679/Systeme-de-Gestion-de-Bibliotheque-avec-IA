07330bd6ea2225bf77d3f4ec524c0402e6bd66ab
selim679 <salimfekih35@gmail.com>
2026-04-28 22:07:26 +0100

    remove secrets from env

--- src/main/java/com/example/librarymanagment/repository/AuthorRepository.java
+++ src/main/java/com/example/librarymanagment/repository/AuthorRepository.java
@@ -3,11 +3,13 @@ package com.example.librarymanagment.repository;
 
 import com.example.librarymanagment.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    @Query("SELECT a FROM Author a ORDER BY a.id DESC")
     List<Author> findTopPopularAuthors();
 }
--- src/main/java/com/example/librarymanagment/service/BookServiceTest.java
+++
@@ -1,4 +0,0 @@
-package com.example.librarymanagment.service;
-
-public class BookServiceTest {
-}

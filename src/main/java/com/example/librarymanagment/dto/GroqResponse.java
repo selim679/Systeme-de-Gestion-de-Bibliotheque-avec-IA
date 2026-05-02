e2c380d4e976c9fc34a9b09ffb52969c09446833
selim679 <salimfekih35@gmail.com>
2026-05-02 15:58:57 +0100

    creation groqrespose

--- src/main/java/com/example/librarymanagment/dto/GroqResponse.java
+++ src/main/java/com/example/librarymanagment/dto/GroqResponse.java
@@ -1,4 +1,15 @@
 package com.example.librarymanagment.dto;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class GroqResponse {
+  private String id;
+  private String object;
+  private long created;
+  private String model;
+  private List<GroqChoice> choices;
+  // Vous pouvez ajouter d'autres champs comme 'usage' si nécessaire
 }

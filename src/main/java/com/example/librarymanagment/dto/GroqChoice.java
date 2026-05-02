3d5d7ee6f5ca2cb335df9ddee14a126e0af5dc1d
selim679 <salimfekih35@gmail.com>
2026-05-02 15:56:40 +0100

    fix gropchoice

--- src/main/java/com/example/librarymanagment/dto/GroqChoice.java
+++ src/main/java/com/example/librarymanagment/dto/GroqChoice.java
@@ -1,4 +1,10 @@
 package com.example.librarymanagment.dto;
 
+import lombok.Data;
+
+@Data
 public class GroqChoice {
+  private int index;
+  private GroqMessage message;
+  private String finish_reason;
 }

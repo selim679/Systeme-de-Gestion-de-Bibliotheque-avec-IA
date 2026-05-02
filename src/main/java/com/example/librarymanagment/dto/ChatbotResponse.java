3e13c9e506d46900ddfd9d6dddaf364ae9827098
selim679 <salimfekih35@gmail.com>
2026-05-02 15:55:22 +0100

    fix chatbotresponse

--- src/main/java/com/example/librarymanagment/dto/ChatbotResponse.java
+++ src/main/java/com/example/librarymanagment/dto/ChatbotResponse.java
@@ -1,12 +1,16 @@
 package com.example.librarymanagment.dto;
 
-
-
+import com.example.librarymanagment.model.Book;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatbotResponse {
-    private String message;
-    private List<BookDTO> recommendedBooks;
+  private String message;
+  private List<Book> recommendedBooks;
 }

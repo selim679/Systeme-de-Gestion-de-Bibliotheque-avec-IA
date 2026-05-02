cf43cca6ccd8ff9bbd3628f2aabf8e2789e5957c
selim679 <salimfekih35@gmail.com>
2026-05-02 16:50:50 +0100

    fix code

--- src/main/java/com/example/librarymanagment/dto/GroqChoice.java
+++ src/main/java/com/example/librarymanagment/dto/GroqChoice.java
@@ -1,10 +1,15 @@
 package com.example.librarymanagment.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class GroqChoice {
   private int index;
   private GroqMessage message;
-  private String finish_reason;
+
+  @JsonProperty("finish_reason")
+  private String finishReason;
 }
